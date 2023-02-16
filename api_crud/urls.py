f30c4a6852736148d2e3e3cf8cd0932978f8531e
Nahid <nd@uberry.ch>
2023-02-16 22:31:11 +0600

    add swagger for api endpoints documentation

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -2,11 +2,23 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 # urls
 urlpatterns = [
     path('api/v1/auth/', include('authentication.urls')),
     path('api/v1/movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
